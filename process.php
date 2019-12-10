213064ccd1bdf53d28e484d14faf41893d05d804
supunsandeeptha <sandeeptha.supun@gmail.com>
2019-12-10 14:08:48 +0530

    adding the alerts and update functionality

--- process.php
+++ process.php
@@ -1,6 +1,14 @@
 <?php
 
+session_start();
+
 $mysqli = new mysqli('localhost', 'root','','crud') or die(mysqli_error($mysqli));
+
+//setting the variables to empty string 
+$name = '';
+$location = '';
+$update = false;
+$id = 0;
 
 if(isset($_POST['save'])){
     $name = $_POST['name'];
@@ -8,6 +16,11 @@ if(isset($_POST['save'])){
     
     $mysqli->query("INSERT INTO data(name, location) VALUES ('$name', '$location')") or
         die($mysqli->error);
+
+        $_SESSION['message'] = "Record has been saved !";
+        $_SESSION['msg_type'] = "success";
+
+        header("location: index.php");
 }
 
 // Checking if the delete button is pressed
@@ -16,7 +29,39 @@ if(isset($_GET['delete'])){
     $id = $_GET['delete'];
     $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
 
+    $_SESSION['message'] = "Record has been deleted !";
+    $_SESSION['msg_type'] = "danger";
 
+    header("location: index.php");
+
+}
+
+// update functionality
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("Select * FROM data WHERE id=$id") or die($mysqli->error());
+    if(count($result)==1){
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+
+
+
+}
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message']= "Record has been updated !";
+    $_SESSION['msg_type'] = "warning";
+
+    header('location: index.php');
 }
 
 ?>
